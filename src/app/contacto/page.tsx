83b417e9fe12fbeb759b3fca71ef5399086a96a2
Luis Rosales <conejoconsal@hotmail.com>
2024-07-21 14:08:07 -0600

    e

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import style from "./page.module.css"
 
 export const metadata: Metadata = {
     title: "True Love Design ! Contacto",
@@ -6,7 +7,21 @@ export const metadata: Metadata = {
   };
 
 export default function Contacto(){
+    
     return(
-        <h1>Contacto</h1>
+        <div>
+            <h1>Contacto</h1>
+            <form action="" className={style.contactForm}>
+                <label htmlFor="firtName">First Name</label>
+                <input type="text" name="firstName" />
+                <label htmlFor="">Last Name</label>
+                <input type="text" name="lastName"/>
+                <input type="text" name="description" />
+                <label htmlFor="">Email</label>
+                <input type="email" name="" id="" />
+
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
     )
 }
